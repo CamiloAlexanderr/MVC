f0039a00b478bc3a334fdde6b6582845f7f07880
institutomexicocancun <camilo.colli@balam.edu.mx>
2019-09-23 12:45:20 -0500

    agregando otra vista

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,6 +25,21 @@ namespace Metodolgia.Controllers
             return View();
         }
 
+        public IActionResult Autos()
+        {
+                    var coches = new List<Coche>
+                    {
+                        new Coche {Marca="BMW",Modelo="X6",Color="Gris"},
+                        new Coche {Marca="Mercedes",Modelo="A200",Color="Blanco"},
+                        new Coche {Marca="Porsche",Modelo="Cayanne",Color="Azul"},
+                        new Coche {Marca="Ford",Modelo="Mustang",Color="Rojo"},
+
+
+                    };
+
+            return View(coches);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -8,4 +8,12 @@ namespace Metodolgia.Models
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
+
+        public class Coche
+    {
+        public string Marca { get;set; }
+        public string Modelo { get;set; }
+        public string Color { get;set; }
+}
+
 }
